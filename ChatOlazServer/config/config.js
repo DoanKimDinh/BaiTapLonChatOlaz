240c5367adc714daaa54d6a7a83c95f2f4458121
duynguyen <gianguyengiaa3@gmail.com>
2020-12-18 16:20:14 +0700

    UpdateAdminThemNguoi

--- ChatOlazServer/config/config.js
+++ ChatOlazServer/config/config.js
@@ -1,6 +1,6 @@
 const AWS_CONFIG = {
     REGION : 'us-east-2',
-    ACCESSID :'',
-    SECRETKEY : ''
+    ACCESSID :'AKIA26GNA4NRFR6WWREE',
+    SECRETKEY : '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'
 }
 module.exports = AWS_CONFIG;
